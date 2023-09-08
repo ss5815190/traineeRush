1bf07478314a59ea9e98ea8e4e4768ce5b17103d
ss5815190 <ss5815190@gmail.com>
2023-09-08 18:00:21 +0800

    linepay e.g

--- src/components/LinePay/LinePay.js
+++ src/components/LinePay/LinePay.js
@@ -6,34 +6,36 @@ const linePayClient = createLinePayClient({
   channelSecretKey: 'c2ce1a7f528021e435d64563b65ae978',
   env: 'development' // env can be 'development' or 'production'
 })
-
-try {
-  const res = await linePayClient.request.send({
-    body: {
-      amount: 1000,
-      currency: 'TWD',
-      orderId: '20211216003',
-      packages: [
-        {
-          id: 'c99abc79-3b29-4f40-8851-bc618ca57856',
+async function sendRequest() {
+    try {
+      const res = await linePayClient.request.send({
+        body: {
           amount: 1000,
-          products: [
+          currency: 'TWD',
+          orderId: '20211216003',
+          packages: [
             {
-              name: 'Product Name',
-              quantity: 2,
-              price: 500
+              id: 'c99abc79-3b29-4f40-8851-bc618ca57856',
+              amount: 1000,
+              products: [
+                {
+                  name: 'Product Name',
+                  quantity: 2,
+                  price: 500
+                }
+              ]
             }
-          ]
+          ],
+          redirectUrls: {
+            confirmUrl: 'https://myshop.com/confirmUrl',
+            cancelUrl: 'https://myshop.com/cancelUrl'
+          }
         }
-      ],
-      redirectUrls: {
-        confirmUrl: 'https://myshop.com/confirmUrl',
-        cancelUrl: 'https://myshop.com/cancelUrl'
-      }
+      })
+  
+      console.log(res)
+    } catch (e) {
+      console.log('error', e)
     }
-  })
-
-  console.log(res)
-} catch (e) {
-  console.log('error', e)
-}
+  }
+  export default sendRequest;
